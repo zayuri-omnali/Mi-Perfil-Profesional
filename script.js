cbbb255a4d3876b1cf949f48a18ec297385cdcf5
zayuri-omnali <apontezayuri@gmail.com>
2026-06-19 14:26:09 -0600

    Add script.js - Interactive functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+    const metaProfesional = "Mi meta profesional es convertirme en una profesional exitosa, contribuyendo positivamente a la sociedad a través de mis habilidades y conocimientos.";
+    alert(metaProfesional);
+}
